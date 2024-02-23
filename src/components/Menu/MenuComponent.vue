cbfd9f378d67e8af6fa9694e0f054868bb4ee9f3
Janana3 <jbruckmo@gmail.com>
2024-02-24 00:44:49 +0100

    Ausklappbare Menupunkte ABER CloseButton kaputt
    keine Ahnung wieso, aber der closebutton geht nicht mehr, vielleicht könnt ihr mir ja sagen wieso

--- src/components/Menu/MenuComponent.vue
+++ src/components/Menu/MenuComponent.vue
@@ -1,7 +1,44 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="overlay" v-if="isOpen" @click="closeMenu"></div>
+      <div class="sidebar" :class="{ open: isOpen }">
+        <button class="close-button" @click="closeMenu">X</button>
+        <ul class="list-group w-75">
+          <li v-for="(item, index) in menuItems" :key="index">
+            <h5 :class="{ 'main-title': !item.subItems, 'sub-menu-title': item.subItems }" @mouseover="hoverEffect(index)">
+              <RouterLink :to="item.link">{{ item.title }}</RouterLink>
+              <span v-if="item.subItems" @click="toggleSubMenu(index)">
+                <span class="expand-icon">v</span>
+              </span>
+            </h5>
+            <ul v-if="item.subItems && openSubMenu === index" class="sub-menu">
+              <li v-for="(subItem, subIndex) in item.subItems" :key="subIndex">
+                <RouterLink :to="subItem.link">{{ subItem.title }}</RouterLink>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
 <script lang="ts">
-import { defineComponent, type PropType } from 'vue'
+import { defineComponent, type PropType, ref } from 'vue'
 import { RouterLink } from 'vue-router'
 import './MenuComponent.css'
+
+interface MenuItem {
+  title: string;
+  link: string;
+  subItems?: SubItem[];
+}
+
+interface SubItem {
+  title: string;
+  link: string;
+}
 
 export default defineComponent({
   name: 'MenuComponent',
@@ -19,148 +56,64 @@ export default defineComponent({
       this.$emit('update:isOpen', false)
     }
   },
+  setup() {
+    const menuItems: MenuItem[] = [
+      { title: 'Home', link: '/' },
+      { title: 'Über uns', link: '/aboutUs', subItems: [
+        { title: 'Auszeichnungen', link: '/auszeichnungen' },
+        { title: 'Geschichte', link: '/geschichte' }
+      ]},
+      { title: 'Mitarbeitende', link: '/mitarbeitende', subItems: [
+        { title: 'Mitarbeitende im Detail', link: '/mitarbeitende-detail' }
+      ]},
+      { title: 'Unsere Spendenprojekte', link: '/spendenprojekte', subItems: [
+        { title: 'Unsere Spendenprojekte im Detail', link: '/spendenprojekte-detail' }
+      ]},
+      { title: 'Produkte', link: '/produkte', subItems: [
+        { title: 'Produkte - Künstler:innen', link: '/produkte-artist' },
+        { title: 'Unsere Produkte im Detail', link: '/produkte-detail' }
+      ]},
+      { title: 'Partner:innen', link: '/partnerinnen', subItems: [
+        { title: 'Partner:innen im Detail', link: '/partnerinnen-detail' }
+      ]},
+      { title: 'Karriere', link: '/karriere', subItems: [
+        { title: 'Jobangebote', link: '/job' },
+        { title: 'Jobangebote im Detail', link: '/job-details' }
+      ]},
+      { title: 'Weiterbildungen', link: '/weiterbildungen', subItems: [
+        { title: 'Weiterbildungen im Detail', link: '/weiterbildungen-detail' }
+      ]},
+      { title: 'Wettbewerbe', link: '/wettbewerbe' },
+      { title: 'Kantine', link: '/kantine', subItems: [
+        { title: 'Allergene', link: '/allergene' }
+      ]},
+      { title: 'Kontakt', link: '/kontakt' },
+      { title: 'Anreise', link: '/anreise' },
+      { title: 'Cookie Richtlinien', link: '/cookie' },
+      { title: 'Datenschutzrichtlinien', link: '/datenschutz' },
+      { title: 'Impressum', link: '/impressum' },
+    ]
+
+    const openSubMenu = ref<number | null>(null)
+
+    const toggleSubMenu = (index: number) => {
+      openSubMenu.value = openSubMenu.value === index ? null : index
+    }
+
+    const closeMenu = () => {
+      openSubMenu.value = null
+      // Emit event to close menu if needed
+    }
+
+    return {
+      menuItems,
+      openSubMenu,
+      toggleSubMenu,
+      closeMenu
+    }
+  },
   components: {
     RouterLink
   }
 })
 </script>
-
-<template>
-  <div>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="overlay" v-if="isOpen" @click="closeMenu"></div>
-      <div class="sidebar" :class="{ open: isOpen }">
-        <button class="close-button" @click="closeMenu">X</button>
-        <ul class="list-group w-75">
-          <li class="main-title">
-            <RouterLink to="/"><h5>Home</h5></RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/kontakt"><h5>Kontakt</h5></RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/impressum"><h5>Impressum</h5></RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/aboutUs">
-              <h5>Über uns</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/auszeichnungen">
-              <h5>Auszeichnungen</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/geschichte">
-              <h5>Geschichte</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/cookie">
-              <h5>Cookie Richtlinien</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/datenschutz">
-              <h5>Datenschutzrichtlinien</h5>
-            </RouterLink>
-          </li>
-          
-          <li class="main-title">
-            <RouterLink to="/kantine">
-              <h5>Kantine</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/allergene">
-              <h5>Allergene</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/karriere">
-              <h5>Karriere</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/job">
-              <h5>Jobangebote</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/job-details">
-              <h5>Jobangebote im Detail</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/anreise">
-              <h5>Anreise</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/mitarbeitende">
-              <h5>Mitarbeitende</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/mitarbeitende-detail">
-              <h5>Mitarbeitende im Detail</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/partnerinnen">
-              <h5>Partner:innen</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/partnerinnen-detail">
-              <h5>Partner:innen im Detail</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/produkte">
-              <h5>Produkte</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/produkte-artist">
-              <h5>Produkte - Künstler:innen</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/produkte-detail">
-              <h5>Unsere Produkte im Detail</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/spendenprojekte">
-              <h5>Unsere Spendenprojekte</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/spendenprojekte-detail">
-              <h5>Unsere Spendenprojekte im Detail</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/weiterbildungen">
-              <h5>Weiterbildungen</h5>
-            </RouterLink>
-          </li>
-          <li class="sub-menu">
-            <RouterLink to="/weiterbildungen-detail">
-              <h5>Weiterbildungen im Detail</h5>
-            </RouterLink>
-          </li>
-          <li class="main-title">
-            <RouterLink to="/wettbewerbe">
-              <h5>Wettbewerbe</h5>
-            </RouterLink>
-          </li>
-          <!-- Weitere Menüpunkte hier -->
-        </ul>
-      </div>
-    </nav>
-
-  </div>
-</template>
