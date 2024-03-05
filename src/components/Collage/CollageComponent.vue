92065f21ee9aef9c9553bb550a5b35d9431207b3
TJD1604 <tim.david1604@gmail.com>
2024-03-05 11:02:34 +0100

    Fix Collage

--- src/components/Collage/CollageComponent.vue
+++ src/components/Collage/CollageComponent.vue
@@ -1,39 +1,47 @@
 <template>
-    <div class="collage">
-      
-      <div v-for="(image, index) in images" :key="index" class="image" :style="{ backgroundImage: image }"></div>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  export default {
-    data() {
-      return {
-        images: [ 'images/Produkte.JPG', 'images/Produkte.JPG', 'images/Produkte.JPG', 'images/Produkte.JPG',  ]
-      };
-    },
-    mounted() {
-      this.shuffleImages();
-    },
-    methods: {
-      // Eine Methode zum Mischen der Bilder
-      shuffleImages() {
-        this.images.sort(() => Math.random() - 0.5);
+  <div class="collage">
+    <div v-for="(image, index) in images" :key="index" class="image" :style="{ 'background-image': 'url(' + image + ')' }"></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      images: [
+        '/public/images/Produkte.JPG',
+        '/public/images/Produkte.JPG',
+        '/public/images/Produkte.JPG',
+        '/public/images/Produkte.JPG'
+      ] as string[]
+    };
+  },
+  mounted() {
+    this.shuffleImages();
+  },
+  methods: {
+    // Eine Methode zum Mischen der Bilder
+    shuffleImages() {
+      // Fisher-Yates Shuffle Algorithmus
+      for (let i = this.images.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.images[i], this.images[j]] = [this.images[j], this.images[i]];
       }
     }
   }
-  </script>
-  
-  <style scoped>
-  .collage {
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Automatische Spaltenanpassung */
-    gap: 10px; /* Abstand zwischen den Bildern */
-  }
-  
-  .image {
-    height: 200px; /* Höhe der Bilder */
-    background-size: cover;
-    background-position: center;
-  }
-  </style>
+}
+</script>
+
+<style scoped>
+.collage {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Automatische Spaltenanpassung */
+  gap: 10px; /* Abstand zwischen den Bildern */
+}
+
+.image {
+  height: 200px; /* Höhe der Bilder */
+  background-size: cover;
+  background-position: center;
+}
+</style>
