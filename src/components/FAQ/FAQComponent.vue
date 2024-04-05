9fdfd7cf5f387fbd0559489e77ade855f137e4d1
Padmoon <kristina.rubin@t-online.de>
2024-04-06 01:40:10 +0200

    FAQ fertig

--- src/components/FAQ/FAQComponent.vue
+++ src/components/FAQ/FAQComponent.vue
@@ -1,60 +1,53 @@
 <template>
-  <button class="accordion">Section 1</button>
-  <div class="panel">
-    <p>Lorem ipsum...</p>
-  </div>
-
-  <button class="accordion">Section 2</button>
-  <div class="panel">
-    <p>Lorem ipsum...</p>
-  </div>
-
-  <button class="accordion">Section 3</button>
-  <div class="panel">
-    <p>Lorem ipsum...</p>
+  <div>
+    <Question
+      v-for="question in questions"
+      :key="question.id"
+      :title="question.title"
+      :info="question.info"
+    />
   </div>
 </template>
 
 <script>
-var acc = document.getElementsByClassName("accordion");
-var i;
+import Question from "./Question.vue";
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-} 
+export default {
+  name: "App",
+  components: {
+    Question,
+  },
+  props: {
+    questions: Array,
+  },
+};
 </script>
 
 <style>
-.accordion {
-  background-color: #eee;
-  color: #444;
-  cursor: pointer;
-  padding: 18px;
-  width: 100%;
-  text-align: left;
-  border: none;
-  outline: none;
-  transition: 0.4s;
+*,
+::after,
+::before {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+ 
+  background: var(--clr-purple);
+  color: var(--clr-grey-1);
+  line-height: 1.5;
+  font-size: 0.875rem;
+}
+
+main {
+  min-height: 100vh;
+  /* using flex because of better browser support */
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 
-/* Style the accordion panel. Note: hidden by default */
-.panel {
-  padding: 0 18px;
-  background-color: white;
-  display: none;
-  overflow: hidden;
-}
+
+
 </style>
--- src/views/aboutUs/FAQView.vue
+++ src/views/aboutUs/FAQView.vue
@@ -16,6 +16,33 @@ export default defineComponent({
     return {
       imagePath: 'images/FAQ.jpg',
       title: 'FAQ',
+      questions: [
+        {
+          id: 1,
+          title: 'Was ist Solidarity Studio?',
+          info: 'Solidarity Studio ist eine gemeinnützige Online-Galerie, die Kunstwerke von Künstlern aus der ganzen Welt präsentiert, um Spenden für wohltätige Zwecke zu sammeln.'
+        },
+        {
+          id: 2,
+          title: 'Wie funktioniert Solidarity Studio?',
+          info: 'Künstler können ihre Werke kostenlos auf Solidarity Studio hochladen. Die Werke werden dann zum Verkauf angeboten, und die Erlöse gehen an ausgewählte gemeinnützige Organisationen, die sich für verschiedene soziale und Umweltthemen einsetzen.'
+        },
+        {
+          id: 3,
+          title: 'Welche Arten von Kunst werden auf Solidarity Studio präsentiert?',
+          info: 'Wir präsentieren eine vielfältige Auswahl an Kunstwerken, darunter Gemälde, Fotografien, Skulpturen, digitale Kunst und mehr. Unser Ziel ist es, eine breite Palette kreativer Ausdrucksformen zu unterstützen.'
+        },
+        {
+          id: 4,
+          title: 'Wie kann ich Solidarity Studio unterstützen?',
+          info: 'Es gibt mehrere Möglichkeiten, Solidarity Studio zu unterstützen. Sie können Kunstwerke kaufen, Spenden tätigen, die Galerie auf Ihren sozialen Medien teilen oder sogar als Künstler beitreten und Ihre eigenen Werke hochladen.'
+        },
+        {
+          id: 5,
+          title: 'Welche Organisationen werden durch Solidarity Studio unterstützt?',
+          info: 'Wir unterstützen eine Vielzahl von Organisationen, die sich für Menschenrechte, Umweltschutz, Bildung, Gesundheitsversorgung und andere wichtige Anliegen einsetzen. Die unterstützten Organisationen variieren je nach den aktuellen Bedürfnissen und Anliegen.'
+        }
+      ],
       options: {
         licenseKey: 'gplv3-license',
         navigation: true,
@@ -43,8 +70,7 @@ export default defineComponent({
           <div class="button-container">
             <h2 class="h2">Häufig gestellte Fragen</h2>
           </div>
-          <FAQComponent/>
-
+          <FAQComponent :questions="questions"  />
         </div>
       </div>
       <div class="section">
@@ -54,9 +80,5 @@ export default defineComponent({
   </div>
 </template>
 <style>
-.stars {
-  font-size: 100px;
-  text-align: center;
-  color: rgb(210, 184, 157);
-}
+
 </style>
