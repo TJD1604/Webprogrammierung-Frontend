e9a4614f76da340599c33b71a35efb4e17c54d79
TJD1604 <tim.david1604@gmail.com>
2024-04-05 15:00:16 +0200

    Karriere

---
+++ src/components/InfoCard/InfoCardComponent.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="flip-card" @mouseenter="flipCard(true)" @mouseleave="flipCard(false)">
+      <div class="flip-card-inner" :class="{ flipped: isFlipped }">
+        <div class="flip-card-front">
+          <div class="content">
+            <slot name="front"></slot>
+          </div>
+        </div>
+        <div class="flip-card-back">
+          <div class="content">
+            <slot name="back"></slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        isFlipped: false
+      };
+    },
+    methods: {
+      flipCard(status) {
+        this.isFlipped = status;
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .flip-card {
+    perspective: 1000px;
+    width: 200px; /* Breite der Karte */
+    height: 300px; /* Höhe der Karte */
+  }
+  
+  .flip-card-inner {
+    width: 100%;
+    height: 100%;
+    transition: transform 0.6s;
+    transform-style: preserve-3d;
+    border: 1px solid #ccc; /* Rand hinzufügen */
+    border-radius: 10px; /* Abgerundete Ecken */
+  }
+  
+  .flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+  }
+  
+  .flip-card-front,
+  .flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+    display: flex;
+    border-radius: 10px; /* Abgerundete Ecken */
+    justify-content: center; /* Horizontal zentrieren */
+    align-items: center; /* Vertikal zentrieren */
+  }
+  
+  .content {
+    text-align: center; /* Inhalt zentrieren */
+  }
+  
+  .flip-card-front {
+    background-color: #fff;
+  }
+  
+  .flip-card-back {
+    background-color: rgb(224, 204, 180);
+    color: white;
+    transform: rotateY(180deg);
+  }
+  </style>
+  
--- src/views/karriere/KarriereView.vue
+++ src/views/karriere/KarriereView.vue
@@ -1,14 +1,61 @@
+<template>
+  <div>
+    <full-page :options="options">
+      <div class="section">
+        <div class="position-relative">
+          <PicBackgroundComponent :imagePath="imagePath" :title="title" />
+        </div>
+      </div>
+      <div class="section">
+        <div class="content-inner" style="width: 90% !important;">
+          <h2 style="align-self: self-start;">Wir als Arbeitgeber</h2>
+          <!-- Karten rendern -->
+          <div class="row">
+            <div class="col-md-8">
+              <div class="row">
+                <div class="col-md-4 mt-5" v-for="(item, index) in cards" :key="index">
+                  <InfoCardComponent>
+                    <template v-slot:front>
+                      <div>
+                        {{ item.front }}
+                      </div>
+                    </template>
+                    <template v-slot:back>
+                      <div>
+                        {{ item.back }}
+                      </div>
+                    </template>
+                  </InfoCardComponent>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4" style="align-self: center; ">
+              <ContactComponent imageText=" Klicke das Herz um selbst ein*e Mitarbeiter*in zu werden" imagePath="/heart.png" style="width: 78%; margin-right: 0; margin-left: auto;"/>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section">
+        <FooterComponent />
+      </div>
+    </full-page>
+  </div>
+</template>
+
 <script lang="ts">
 import { defineComponent } from "vue";
 import FooterComponent from '@/components/Footer/FooterComponent.vue'
-
+import InfoCardComponent from '@/components/InfoCard/InfoCardComponent.vue'
 import PicBackgroundComponent from "@/components/PicBackground/PicBackgroundComponent.vue";
+import ContactComponent from "@/components/ContactComponent/ContactComponent.vue";
 import '../view.css'
 export default defineComponent({
   name: "KarriereView",
   components: {
     FooterComponent,
-    PicBackgroundComponent
+    PicBackgroundComponent,
+    InfoCardComponent,
+    ContactComponent
   },
   data() {
     return {
@@ -19,26 +66,16 @@ export default defineComponent({
         navigationTooltips: ['1', '2', '3']
       },
       imagePath: 'images/Karriere.JPG',
-      title: 'Karriere'
+      title: 'Karriere',
+      cards: [
+        { front: 'Mobiles Arbeiten', back: 'Nähere Informationen zum mobilen Arbeiten' },
+        { front: 'Obst am Arbeitsplatz', back: 'Nähere Informationen zum Obst am Arbeitsplatz' },
+        { front: 'Weiterbildungsmöglichkeiten', back: 'Nähere Informationen zu Weiterbildungsmöglichkeiten' },
+        { front: 'Flexible Arbeitszeiten', back: 'Nähere Informationen zu flexiblen Arbeitszeiten' },
+        { front: 'Betriebliche Gesundheitsförderung', back: 'Nähere Informationen zur betrieblichen Gesundheitsförderung' },
+        { front: 'Mitarbeiter-Events', back: 'Nähere Informationen zu Mitarbeiter-Events' }
+      ]
     };
   }
 });
 </script>
-
-<template>
-  <div>
-    <full-page :options="options">
-      <div class="section">
-        <div class="position-relative">
-          <PicBackgroundComponent :imagePath="imagePath" :title="title" />
-        </div>
-      </div>
-      <div class="section">
-        <div class="content-inner"></div>
-      </div>
-      <div class="section">
-        <FooterComponent />
-      </div>
-    </full-page>
-  </div>
-</template>
