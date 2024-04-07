f96dff6acdcd21736109dc8c6aff264034da4c80
Padmoon <kristina.rubin@t-online.de>
2024-04-07 15:54:04 +0200

    Weiterbildungen + passend Componente

---
+++ src/components/LearnCard/LearnCardComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card" >
+      <div class="card-content">
+        <h3 class="card-name">{{ headline }}</h3>
+        <p><i>Was?</i><br> {{ description }}</p>
+        <p class="time"><i>Wann?</i><br> {{ timeframe }}</p>
+
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      headline: String,
+      description: String,
+      timeframe: String
+      
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .card {
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    overflow: hidden;
+    width: 300px;
+    height: auto;
+    border: 2px solid rgb(210, 184, 157);
+  }
+
+  
+  .card-content {
+    padding: 20px;
+    align-self: center;
+    justify-self: center;
+
+  }
+
+ 
+  </style>
+  
--- src/views/weiterbildungen/WeiterbildungenView.vue
+++ src/views/weiterbildungen/WeiterbildungenView.vue
@@ -1,14 +1,16 @@
 <script lang="ts">
-import { defineComponent } from "vue";
-import PicBackgroundComponent from "@/components/PicBackground/PicBackgroundComponent.vue";
+import { defineComponent } from 'vue'
+import PicBackgroundComponent from '@/components/PicBackground/PicBackgroundComponent.vue'
 import FooterComponent from '@/components/Footer/FooterComponent.vue'
+import LearnCardComponent from '@/components/LearnCard/LearnCardComponent.vue'
 
 import '../view.css'
 export default defineComponent({
-  name: "WeiterbildungenView",
+  name: 'WeiterbildungenView',
   components: {
     FooterComponent,
-    PicBackgroundComponent
+    PicBackgroundComponent,
+    LearnCardComponent
   },
   data() {
     return {
@@ -20,9 +22,9 @@ export default defineComponent({
       },
       imagePath: 'images/Weiterbildung.jpg',
       title: 'Weiterbildungen'
-    };
+    }
   }
-});
+})
 </script>
 
 <template>
@@ -34,7 +36,34 @@ export default defineComponent({
         </div>
       </div>
       <div class="section">
-        <div class="content-inner"></div>
+        <div class="content-inner">
+          <div class="button-container">
+            <h2>Kostenlose Weiterbildungen</h2>
+            <p>Entdecke unsere vielfältigen kostenlosen künstlerischen Weiterbildungen und erweitere deine kreativen Fähigkeiten ohne Kosten. Tauche ein in die Welt der Malerei, Keramik, Fotografie und Schauspiel und entwickle deine Fertigkeiten unter der Anleitung erfahrener Experten. Nutze diese Gelegenheit, um deine Leidenschaft für Kunst zu entfalten und neue Horizonte zu erkunden.</p>
+          </div>
+          <div class="button-container">
+            <LearnCardComponent
+              headline="Malerei"
+              description="Entdecke die Welt der Malerei und erweitere deine künstlerischen Fähigkeiten. Lerne verschiedene Techniken, Farbpaletten und Stile kennen, während du in einer inspirierenden Umgebung arbeitest. Dieser Workshop bietet dir die Möglichkeit, deine Kreativität auszudrücken und neue Kunstwerke zu schaffen."
+              timeframe="Mo 21.06.2024"
+            />
+            <LearnCardComponent
+              headline="Keramik"
+              description="Tauche ein in die Kunst der Keramik und lerne, wie du deine eigenen Kunstwerke gestalten kannst. Erfahre mehr über die verschiedenen Tonarten, Formgebungstechniken und Brennmethoden, um einzigartige Keramikobjekte zu schaffen. Dieser Kurs bietet dir die Möglichkeit, deine handwerklichen Fähigkeiten zu verbessern und dein eigenes Geschirr, Vasen und Skulpturen herzustellen."
+              timeframe="Di 22.06.2024"
+            />
+            <LearnCardComponent
+              headline="Fotografie"
+              description="Erforsche die Grundlagen der Fotografie und entwickle deine Fertigkeiten in der Bildkomposition. Lerne, wie du mit Licht, Schärfe und Perspektive spielst, um fesselnde Bilder zu erstellen. Dieses Seminar bietet dir die Möglichkeit, die Welt durch die Linse zu betrachten und deine fotografischen Fähigkeiten zu verbessern."
+              timeframe="Mi 23.06.2024"
+            />
+            <LearnCardComponent
+              headline="Schauspielkurs"
+              description="Entdecke die Welt des Schauspiels und lerne, wie du deine emotionale Bandbreite erweiterst und deine schauspielerischen Fähigkeiten entwickelst. Tauche ein in verschiedene Schauspieltechniken, von der Improvisation bis zur Charakterentwicklung, und arbeite an Szenen und Monologen. Dieser Kurs bietet dir die Möglichkeit, dein Talent auf der Bühne zu entfalten und eine starke Präsenz als Schauspieler zu entwickeln."
+              timeframe="Do 24.06.2024"
+            />
+          </div>
+        </div>
       </div>
       <div class="section">
         <FooterComponent />
