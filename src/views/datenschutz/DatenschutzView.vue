f978e9c8af5d5a6c68f35e4e7047753107325194
Janana3 <jbruckmo@gmail.com>
2024-04-07 18:32:18 +0200

    datenschutz

--- src/views/datenschutz/DatenschutzView.vue
+++ src/views/datenschutz/DatenschutzView.vue
@@ -45,9 +45,19 @@ export default defineComponent({
           <div class="textdev">
              <!-- Privacy Policy -->
             <h4>Allgemeiner Hinweis und Pflichtinformationen</h4>
-            <h5>Benennung der verantwortlichen Stelle</h5>
-            <p>Die verantwortliche Stelle entscheidet allein oder gemeinsam mit anderen über die Zwecke und Mittel der Verarbeitung von personenbezogenen Daten (z.B. Namen, Kontaktdaten o. Ä.).</p>
-            <h5>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h5>
+            <h5>Benennung der verantwortlichen Stelle</h5><p>
+              Solidarity Studio <br />
+              Alt-Friedrichsfelde 60 <br />10315 Berlin
+            </p>
+            <h2 class="subheading">Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV:</h2>
+            <p>Solidarity Studio<br />Alt-Friedrichsfelde 60, 10315 Berlin</p>
+            <h2 class="subheading">Kontakt:</h2>
+            <p>
+              Telefon: +49 1234 56789<br />E-Mail:
+              <a class="maillink">
+                s_bruckmoser22@hwr-berlin.de</a
+              >
+            </p><h5>Widerruf Ihrer Einwilligung zur Datenverarbeitung</h5>
             <p>Nur mit Ihrer ausdrücklichen Einwilligung sind einige Vorgänge der Datenverarbeitung möglich. Ein Widerruf Ihrer bereits erteilten Einwilligung ist jederzeit möglich. Für den Widerruf genügt eine formlose Mitteilung per E-Mail. Die Rechtmäßigkeit der bis zum Widerruf erfolgten Datenverarbeitung bleibt vom Widerruf unberührt.</p>
             <h5>Recht auf Beschwerde bei der zuständigen Aufsichtsbehörde</h5>
             <p>Als Betroffener steht Ihnen im Falle eines datenschutzrechtlichen Verstoßes ein Beschwerderecht bei der zuständigen Aufsichtsbehörde zu. Zuständige Aufsichtsbehörde bezüglich datenschutzrechtlicher Fragen ist der Landesdatenschutzbeauftragte des Bundeslandes, in dem sich der Sitz unseres Unternehmens befindet. Der folgende Link stellt eine Liste der Datenschutzbeauftragten sowie deren Kontaktdaten bereit: <a href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html" target="_blank">https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html</a>.</p>
